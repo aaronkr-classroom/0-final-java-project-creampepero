9e4f8a10bc5a95bce5526163c0d7f14b58523c06
creampepero <pyy_0413@naver.com>
2024-03-29 13:56:46 +0900

    3월 26일 과제 제출

--- src/Welcome.java
+++ src/Welcome.java
@@ -1,8 +1,12 @@
 import java.util.Scanner;
 
 public class Welcome {
+	static final int NUM_BOOK = 3;
+	static final int NUM_ITEM = 7;
 
 	public static void main(String[] args) {
+		String[][]mBook = new String[NUM_BOOK][NUM_ITEM];
+		
 		Scanner input = new Scanner(System.in);
 		
 		System.out.print("당신의 이름을 입력하세요: ");
@@ -53,19 +57,19 @@ public class Welcome {
 					menuCartItemclear();
 					break;
 				case 4:
-					menuCartAddItem();
+					menuCartBill();
 					break;
 				case 5:
-					menuCartRemoveItemCount();
+					menuCartAddItem(mBook);
 					break;
 				case 6:
-					menuCartRemoveItem();
+					menuCartRemoveItemCount();
 					break;
 				case 7:
-					menuCartItemBill();
-					break;
+					menuCartRemoveItem();
+					break;				
 				case 8:
-					menuCartItemExit();
+					menuCartExit();
 					quit = true;
 					break;
 						
@@ -81,10 +85,10 @@ public class Welcome {
 	 *   
 	 */
 	public static void menuIntro () {
-		System.out.println("1. 고객 정보 확인하기 \t4. 바구니에 항목 추가하기");
-		System.out.println("2. 장바구니 상품 목록 보기 \t5. 장바구니의 항목 수량 줄이기");
-		System.out.println("3. 장바구니 비우기 \t\t6. 장바구니의 항목 삭제하기");
-		System.out.println("7. 영수증 표시하기 \t\t8. 종료");
+		System.out.println("1. 고객 정보 확인하기 \t5. 바구니에 항목 추가하기");
+		System.out.println("2. 장바구니 상품 목록 보기 \t6. 장바구니의 항목 수량 줄이기");
+		System.out.println("3. 장바구니 비우기 \t\t7. 장바구니의 항목 삭제하기");
+		System.out.println("4. 영수증 표시하기 \t\t8. 종료");
 		System.out.println("*********************************************");
 		}
 	
@@ -113,19 +117,82 @@ public class Welcome {
 	public static void menuCartItemclear() {
 		System.out.println("3. 장바구니 비우기");
 }
-	public static void menuCartAddItem() {
-		System.out.println("4. 장바구니에 항목 추가하기 : ");
+	public static void menuCartBill() {
+		System.out.println("4. 영수증 표시하기");
+}
+	public static void menuCartAddItem(String[][]book) {
+		// System.out.println("5. 장바구니에 항목 추가하기 : ");
+		BookList(book);
+		for (int i = 0; i < NUM_BOOK; i++) {
+			for(int j = 0; j < NUM_BOOK;  j++)
+				System.out.print(book[i][j] + " | ");
+			System.out.println("");
+		}
+		boolean quit = false;
+		
+		while (!quit) {
+			System.out.print("장바구니에 추가할 도서의 ID를 입력하세요:");
+			
+		Scanner input = new Scanner(System.in);
+		String str = input.nextLine();
+		
+		boolean flag = false;
+		int numID = -1;
+		
+		for(int i = 0; i < NUM_BOOK; i++) {
+			if (str.equals(book[i][0])) {
+				numID = i;
+				flag = true;
+				break;
+			}
+		}
+			if (flag) {
+				System.out.println("장바구니에 추가하겠습니까? Y | N ");
+				str = input.nextLine();
+				
+				if (str.toUpperCase().equals("Y")) {
+					System.out.println(book[numID][0] + "도서가 장바구니에 추가되었습니다.");
+				}
+				quit = true;
+			} else
+				System.out.println("다시 입력해 주세요");
+		} // while 끝
 }
 	public static void menuCartRemoveItemCount() {
-		System.out.println("5. 장바구니의 항목 수량 줄이기");
+		System.out.println("6. 장바구니의 항목 수량 줄이기");
 }
 	public static void menuCartRemoveItem() {
-		System.out.println("6. 장바구니의 항목 삭제하기");
+		System.out.println("7. 장바구니의 항목 삭제하기");
 }
-	public static void menuCartItemBill() {
-		System.out.println("7. 영수증 표시하기");
-	}
-	public static void menuCartItemExit() {
+	public static void menuCartExit() {
 		System.out.println("8. 종료");
 }
+	public static void BookList(String[][]book) {
+		
+		book[0][0] = "ISBN1234";
+		book[0][1] = "쉽게 배우는 JSP 웹 프로그래";
+		book[0][2] = "27000";
+		book[0][3] = "송미영";
+		book[0][4] = "단계별로 쇼핑몰을 구현하며 배우는 JSP 웹 프로그래밍";
+		book[0][5] = "IT전문서";
+		book[0][6] = "2018/10/08";
+		
+		book[1][0] = "ISBN1235";
+		book[1][1] = "안드로이드 프로그래";
+		book[1][2] = "33000";
+		book[1][3] = "우재남";	
+		book[1][4] = "실습 단계별 명쾌한 멘토링";
+		book[1][5] = "IT전문서";
+		book[1][6] = "2022/01/22";
+								
+		book[2][0] = "ISBN1236";
+		book[2][1] = "스크래치";
+		book[2][2] = "22000"; 
+		book[2][3] = "고광일";
+		book[2][4] = "컴퓨팅 사고력을 키우는 블록 코딩";
+		book[2][5] = "컴퓨터 입문";
+		book[2][6] = "2019/06/10";
+		
+	}
+	
 	} //welcome class 끝
